48d87d8b7b689ad5eed342b5eb305ded9753d51e
Aadarsh Singh <aadarsh4492@gmail.com>
2024-12-07 19:24:38 +0530

    Add files via upload

---
+++ Week_05-Fundamentals_in_Python_for_Coding/55-Importing_Modules.py
@@ -0,0 +1,13 @@
+import math
+print(math.sqrt(16))
+
+from math import sqrt
+print(sqrt(255))
+
+from math import sqrt as s
+print(s(2255))
+
+from math import *
+print(sqrt(1231))
+
+help(math)
---
+++ Week_05-Fundamentals_in_Python_for_Coding/56-Introduction_to_NumPy_and_Pandas.py
@@ -0,0 +1,11 @@
+# How to import numpy
+pip install numpy # to install numpy
+import numpy #method-1
+import numpy as np #method-2 standard method
+np.__version____ # Will tell the version of numpy
+
+# How to import pandas
+pip install pandas # to install pandas
+import pandas #method-1
+import pandas as pd #method-2 standard method
+pd.__version____ # Will tell the version of pandas
